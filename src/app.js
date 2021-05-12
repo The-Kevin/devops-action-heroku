fe52f13957a35a859e1f2270e334f5cba0e9b114
Kevin Almeida <kevin.almeida1111.ka@gmail.com>
2021-05-12 10:10:19 -0300

    start

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  return res.json({
+    name: "DevOps Action!",
+    tools: {
+      Express: true,
+      NodeJs: true,
+      CircleCI: true,
+      Heroku: true,
+    },
+    author: "Kevin Almeida",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`running in ${PORT}`);
+});
