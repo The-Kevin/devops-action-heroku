562854edff3c164262eccff831ee208ca9b7112c
Kevin Almeida <kevin.almeida1111.ka@gmail.com>
2021-05-12 12:47:05 -0300

    test2

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
       NodeJs: true,
       CircleCI: true,
       Heroku: true,
-      Tested: false,
+      Tested: true,
     },
     author: "Kevin Almeida",
   });
